e95d8d963ba3fc43d0f9a2af5a692fad6772c00f
chenlinchao <chenlinchao@yihecloud.com>
2017-09-28 14:34:10 +0800

    BeanDefinitionRegistryPostProcessor and BeanFactoryPostProcessor modify beanDifinition

---
+++ src/main/java/com/springsource/xmlparser/Student.java
@@ -0,0 +1,42 @@
+package com.springsource.xmlparser;
+
+/**
+ * Description
+ *
+ * @author chenlinchao
+ * @version 1.0
+ * @date 2017-09-25
+ * Copyright 青海粮食云项目组
+ */
+public class Student {
+
+    private String username;
+
+    private String password;
+
+    private String school;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+}
---
+++ src/test/java/com/springsource/xmlparser/XmlParserTest.java
@@ -0,0 +1,39 @@
+package com.springsource.xmlparser;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Description
+ *
+ * @author chenxyz
+ * @version 1.0
+ * @date 2017-09-25
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring.xml")
+public class XmlParserTest implements ApplicationContextAware {
+
+    public ApplicationContext context;
+
+    @Test
+    public void test1(){
+
+        Student s = (Student)context.getBean("jackstudent");
+
+        System.out.println(s.getUsername());
+        System.out.println(s.getPassword());
+        System.out.println(s.getSchool());
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext arg0)
+            throws BeansException {
+        this.context = arg0;
+    }
+}
